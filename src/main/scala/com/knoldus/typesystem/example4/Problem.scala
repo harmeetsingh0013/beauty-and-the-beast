e8800967d6502a15b0734a45434a32ce15b459e8
HarmeetSingh <harmeet@knoldus.com>
2019-01-12 23:40:13 +0530

    refactor functors in example 4

--- src/main/scala/com/knoldus/typesystem/example4/Problem.scala
+++ src/main/scala/com/knoldus/typesystem/example4/Problem.scala
@@ -5,25 +5,27 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 object Problem extends App {
 
-    trait Functor[A, F[_]] {
-        def map[B](x: F[A])(f: A => B): F[B]
+    trait Functor [F[_]] {
+        def map[A, B](x: F[A])(f: A => B): F[B]
     }
-
-    def optionFunctor[A]: Functor[A, Option] = new Functor[A, Option] {
-        override def map[B](x : Option[A])(f : A => B) : Option[B] = x.map(f)
+    def optionFunctor[A]: Functor[Option] = new Functor[Option] {
+        override def map[A, B](x : Option[A])(f : A => B) : Option[B] = x.map(f)
     }
 
     println("Option Functor: " + optionFunctor.map(Some(9))(_ + 20))
 
-    def futureFunctor[A]: Functor[A, Future] = new Functor[A, Future] {
-        override def map[B](x : Future[A])(f : A => B) : Future[B] = x.map(f)
+    def futureFunctor[A]: Functor[Future] = new Functor[Future] {
+        override def map[A, B](x : Future[A])(f : A => B) : Future[B] = x.map(f)
     }
 
-    println("Future Functor: " + futureFunctor.map(Future.successful(3))(_ + 5))
+    val result = futureFunctor.map(Future.successful(3))(_ + 5)
+    Thread.sleep(1000)
 
-   /* def mapFunctor[X, A]: Functor[A, Map] = new Functor[A, Map] {
-        override def map[B](x : Map[X, A])(f : A => B) : Map[X, B] = x.map(f)
+    println("Future Functor: " + result)
+
+/*    def mapFunctor[X, A]: Functor[Map] = new Functor[Map] {
+        override def map[A, B](x : Map[X, A])(f : A => B) : Map[X, B] = x.map(f)
     }*/
 
-    Thread.sleep(5000)
+
 }
--- src/main/scala/com/knoldus/typesystem/example4/Solution.scala
+++ src/main/scala/com/knoldus/typesystem/example4/Solution.scala
@@ -2,43 +2,40 @@ package com.knoldus.typesystem.example4
 
 object Solution extends App {
 
-    trait Functor[A, F[_]] {
-        def map[B](x: F[A])(f: A => B): F[B]
+    trait Functor [F[_]] {
+        def map[A, B](x: F[A])(f: A => B): F[B]
     }
 
     // The Solution of the Functor using Map is:
     def mapFunctor[X, A] = {
         type Alias[A] = Map[X, A]
-        new Functor[A, Alias] {
-            override def map[B](x : Alias[A])(f : A => B) : Alias[B] = x.map{
+        new Functor[Alias] {
+            override def map[A, B](x : Alias[A])(f : A => B) : Alias[B] = x.map{
                 case (key, value) => (key, f(value))
             }
         }
     }
 
     // How we can call the mapFunctor function?
-
 //    mapFunctor[String, Int].map(Map("Key" -> 4))(_ + 8)
 
     // Question: Do we have any other Solution???
     // Answer: Type Lambda
-    def mapFunctorTypeLambda[X, A]: Functor[A, ({ type Alias[A] = Map[X, A]})#Alias ] =
-        new Functor[A, ({ type Alias[A] = Map[X, A]})#Alias] {
-            override def map[B](x : Map[X, A])(f : A => B) : Map[X, B] = x.map{
-                case (key, value) => (key, f(value))
-            }
+    def mapFunctorTypeLambda[X, A]: Functor[ ({ type Alias[A] = Map[X, A]})#Alias ] =
+        new Functor[ ({ type Alias[A] = Map[X, A]})#Alias ] {
+        override def map[A, B](x : Map[X, A])(f : A => B) : Map[X, B] = x.map{
+            case (key, value) => (key, f(value))
         }
-
+    }
     println("Map Functor: " + mapFunctorTypeLambda[String, Int].map(Map("Key" -> 4))(_ + 10))
 
 
     // Questions: Do we have any other way or Clean syntax for achive same thing
     // Answer: Yes, Kind Projector Plugin
-    def mapFunctorKindProjector[X, A]: Functor[A, Map[X, ?]] = new Functor[A, Map[X, ?]] {
-        override def map[B](x : Map[X, A])(f : A => B) : Map[X, B] = x.map{
+        def mapFunctorKindProjector[X, A] = new Functor[Map[X, ?]] {
+        override def map[A, B](x : Map[X, A])(f : A => B) : Map[X, B] = x.map{
             case (key, value) => (key, f(value))
         }
     }
-
     println("Map Functor: " + mapFunctorKindProjector[String, Int].map(Map("Key" -> 4))(_ + 10))
 }
