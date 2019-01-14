6a2f5b9f08496ff3dd7a3b19118918f52521bd5e
HarmeetSingh <harmeet@knoldus.com>
2019-01-14 14:02:30 +0530

    refactor bit examples

--- src/main/scala/com/knoldus/typesystem/example4/Solution.scala
+++ src/main/scala/com/knoldus/typesystem/example4/Solution.scala
@@ -7,7 +7,7 @@ object Solution extends App {
     }
 
     // The Solution of the Functor using Map is:
-    def mapFunctor[X, A] = {
+    def mapFunctor[X] = {
         type Alias[A] = Map[X, A]
         new Functor[Alias] {
             override def map[A, B](x : Alias[A])(f : A => B) : Alias[B] = x.map{
@@ -17,25 +17,25 @@ object Solution extends App {
     }
 
     // How we can call the mapFunctor function?
-//    mapFunctor[String, Int].map(Map("Key" -> 4))(_ + 8)
+    mapFunctor[String].map(Map("Key" -> 4))(_ + 8)
 
     // Question: Do we have any other Solution???
     // Answer: Type Lambda
-    def mapFunctorTypeLambda[X, A]: Functor[ ({ type Alias[A] = Map[X, A]})#Alias ] =
+    def mapFunctorTypeLambda[X]: Functor[ ({ type Alias[A] = Map[X, A]})#Alias ] =
         new Functor[ ({ type Alias[A] = Map[X, A]})#Alias ] {
         override def map[A, B](x : Map[X, A])(f : A => B) : Map[X, B] = x.map{
             case (key, value) => (key, f(value))
         }
     }
-    println("Map Functor: " + mapFunctorTypeLambda[String, Int].map(Map("Key" -> 4))(_ + 10))
+    println("Map Functor: " + mapFunctorTypeLambda[String].map(Map("Key" -> 4))(_ + 10))
 
 
     // Questions: Do we have any other way or Clean syntax for achive same thing
     // Answer: Yes, Kind Projector Plugin
-        def mapFunctorKindProjector[X, A] = new Functor[Map[X, ?]] {
+        def mapFunctorKindProjector[X] = new Functor[Map[X, ?]] {
         override def map[A, B](x : Map[X, A])(f : A => B) : Map[X, B] = x.map{
             case (key, value) => (key, f(value))
         }
     }
-    println("Map Functor: " + mapFunctorKindProjector[String, Int].map(Map("Key" -> 4))(_ + 10))
+    println("Map Functor: " + mapFunctorKindProjector[String].map(Map("Key" -> 4))(_ + 10))
 }
