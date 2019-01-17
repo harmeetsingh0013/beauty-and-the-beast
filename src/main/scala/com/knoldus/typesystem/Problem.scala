97547470b5ea40c689cb8d8e02823a7b7a92d6a7
HarmeetSingh <harmeet@knoldus.com>
2019-01-17 07:18:45 +0530

    commit problem 1 and solution

--- src/main/scala/com/knoldus/typesystem/Problem.scala
+++ src/main/scala/com/knoldus/typesystem/Problem.scala
@@ -1,6 +1,35 @@
 package com.knoldus.typesystem
 
-object Problem
-{
+object Problem extends App {
 
+    // Require method perform Addition on Float
+    def sumFloat(a: Float, b: Float) : Float = a + b
+
+    sumFloat(3.2f, 3.2f)
+
+    // Require methods perform addition on any number
+    def sumAnyVal(a: AnyVal, b: AnyVal):AnyVal = (a, b) match {
+        case (a: Int, b: Int) => a + b
+        case (a: Float, b: Float) => a + b
+        case (a: Double, b: Double) => a + b
+        case _ => 0
+    }
+
+    sumAnyVal(1, 2)
+    sumAnyVal(1.2f, 2.3f)
+    sumAnyVal(1.0, 2.3)
+
+    // Require method perform additions on numbers and on String as well.
+    def sumAny(a: Any, b: Any):Any = (a, b) match {
+        case (a: Int, b: Int) => a + b
+        case (a: Double, b: Double) => a + b
+        case (a: String, b: String) => a + b
+        case _ => "match error"
+    }
+
+    sumAny(1, 2)
+    sumAny(2.0, 4.3)
+    sumAny("One", "Two")
+
+    // Question: What are the problems we are facing during sumAnyVal and sumAny methods???
 }
--- src/main/scala/com/knoldus/typesystem/Solution.scala
+++ src/main/scala/com/knoldus/typesystem/Solution.scala
@@ -1,6 +1,34 @@
 package com.knoldus.typesystem
 
-object Solution
-{
+object Solution extends App {
 
+    // Create an abstractions which contains action declaration
+    trait Adder[T] {
+        def sum(a: T, b: T): T
+    }
+
+    // Create an public interface(not java interface) by whom, we perform our action
+    def sum[T](a: T, b: T)(implicit adder: Adder[T]): T = adder.sum(a, b)
+
+    // provide an implementation of abstraction, to whom you require
+    // -- Int implementation
+    implicit val integer = new Adder[Int] {
+        override def sum(a: Int, b: Int): Int = a + b
+    }
+
+    sum(2, 4)
+
+    // -- String implementation
+    implicit val stringAdder = new Adder[String] {
+        override def sum(a : String, b : String) : String = a + b
+    }
+
+    sum("One", "Two")
+
+    // -- List[Int] implementation
+    implicit val listAdder = new Adder[List[Int]] {
+        override def sum(a : List[Int], b : List[Int]) : List[Int] =
+            (a zip b).map { case (x, y) => x + y}
+    }
+    sum(List(1, 3, 4, 5), List(3, 3, 4, 5))
 }
