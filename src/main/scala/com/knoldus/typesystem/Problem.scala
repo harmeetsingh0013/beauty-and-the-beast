8a313e7284feda8835fa54bee245720be148ab51
HarmeetSingh <harmeet@knoldus.com>
2019-01-17 07:28:45 +0530

    commit problem 3 and solution

--- src/main/scala/com/knoldus/typesystem/Problem.scala
+++ src/main/scala/com/knoldus/typesystem/Problem.scala
@@ -1,6 +1,46 @@
 package com.knoldus.typesystem
 
-object Problem
-{
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
+object Problem extends App {
+
+    def stringLength: String => Int = _.length
+
+    def lengthOfString(string: String): Int = string.length
+
+    // Performing the same operation, but outer wrapper is change
+    def lengthOfOptionString(option: Option[String]): Option[Int] = option.map(_.length)
+
+    // Performing the same operation, but outer wrapper is change
+    def lengthOfFutureString(future: Future[String]): Future[Int] = future.map(_.length)
+
+    lengthOfString("Harmeet")
+    lengthOfOptionString(Some("Singh"))
+    lengthOfFutureString(Future.successful("Knoldus"))
+
+    // Let's create generic method for above operations
+
+    //    def genericLengthOfString[T](string: T)(implicit f: Functor[T]) = {}
+
+
+
+    /**
+      * Why Functor Require F[_] ???
+      *
+      * --- Pass String
+      * def genericLengthOfString[String](string: String)(implicit f: Functor[String])  {
+      *
+      * }
+      *
+      * --- Pass Option[String]
+      * def genericLengthOfString[Option[String]](string: Option[String])(implicit f: Functor[Option[String]]) = {
+      *
+      * }
+      *
+      * --- Pass Future[String]
+      * def genericLengthOfString[Future[String]](string: Future[String])(implicit f: Functor[Future[String]]) = {
+      *
+      * }
+      */
 }
--- src/main/scala/com/knoldus/typesystem/Solution.scala
+++ src/main/scala/com/knoldus/typesystem/Solution.scala
@@ -1,6 +1,30 @@
 package com.knoldus.typesystem
 
-object Solution
-{
+import cats.{Functor, Id}
+import cats.implicits._
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
 
+object Solution extends App {
+
+    def stringLength: String => Int = _.length
+
+    def genericLengthOfString[F[_]](string: F[String])(implicit f: Functor[F]): F[Int] = {
+        f.map(string)(stringLength)
+    }
+
+    genericLengthOfString[Option](Some("Singh"))
+    genericLengthOfString[Future](Future.successful("Knoldus"))
+
+    // How can we pass normal string value rather than any wrapped one ???
+    genericLengthOfString[Id]("Harmeet")
+
+    // Another syntax of creating generic method
+    def genericLengthOfStringNew[F[_]: Functor](string: F[String]): F[Int] = {
+        string.map(stringLength)
+    }
+
+    genericLengthOfStringNew[Id]("Harmeet")
+    genericLengthOfStringNew[Option](Some("Singh"))
+    genericLengthOfStringNew[Future](Future.successful("Knoldus"))
 }
