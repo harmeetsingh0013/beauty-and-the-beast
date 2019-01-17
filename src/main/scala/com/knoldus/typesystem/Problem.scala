4eb765fdd2e4d325f309ce10626308bc3c5a2dbb
HarmeetSingh <harmeet@knoldus.com>
2019-01-17 07:21:00 +0530

    commit problem 2 and solution

--- src/main/scala/com/knoldus/typesystem/Problem.scala
+++ src/main/scala/com/knoldus/typesystem/Problem.scala
@@ -1,6 +1,40 @@
 package com.knoldus.typesystem
 
-object Problem
-{
+object Problem extends App {
+
+    trait Food
+    trait FastFood extends Food
+    trait HealthyFood extends Food
+
+    class Burger extends FastFood
+    class Noodles extends FastFood
+
+    class Vegetables extends HealthyFood
+    class Fruits extends HealthyFood
+
+    case class Box(food: Food)
+
+    class Store {
+        def orderYourHealthyFood(food: String): Box = food match {
+            case "Vegetables" => Box(new Vegetables)
+            case "Fruits" => Box(new Fruits)
+        }
+
+        // What, if Noodles are Out Of Order???
+        // OR
+        // From Now onwards, I will be only working on burgers
+        def orderYourFastFood(fastFood: String): Box = fastFood match {
+            case "Burger" => Box(new Burger)
+            //        case "Noodles" => Box(new Noodles) // Out of order
+            case _ => throw new RuntimeException
+        }
+    }
+
+    val store = new Store
+    store.orderYourHealthyFood("Vegetables")
+    store.orderYourHealthyFood("Fruits")
+
+    store.orderYourFastFood("Burger")
+    //store.orderYourFastFood("Noodles")
 
 }
--- src/main/scala/com/knoldus/typesystem/Solution.scala
+++ src/main/scala/com/knoldus/typesystem/Solution.scala
@@ -1,6 +1,25 @@
 package com.knoldus.typesystem
 
-object Solution
-{
+object Solution extends App {
 
+    trait Food
+    trait FastFood extends Food
+    trait HealthyFood extends Food
+
+    class Burger extends FastFood
+    class Noodles extends FastFood
+
+    class Vegetables extends HealthyFood
+    class Fruits extends HealthyFood
+
+    case class Box(food: Food)
+
+    class Store {
+        def orderYourFastFood[T >: Burger <: Burger]: Box = Box(new Burger)
+    }
+
+    val store = new Store
+    store.orderYourFastFood[Burger]
+    //    store.orderYourFastFood[Noodles]
+    //    store.orderYourFastFood[FastFood]
 }
